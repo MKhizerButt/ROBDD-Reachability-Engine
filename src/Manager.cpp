3e399052366f7b6d5e1509bc872f6f0368d7ae16
M Khizer Butt <mkhizerbutt@gmail.com>
2025-12-08 13:06:48 +0100

    Test Passed: Integration test

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -125,7 +125,7 @@ namespace ClassProject {
     BDD_ID Manager::coFactorFalse(BDD_ID f){ return nodes[f].low; }
 
     BDD_ID Manager::and2(BDD_ID a, BDD_ID b) { return ite(a, b, FALSE_ID); }
-    BDD_ID Manager::or2(BDD_ID a, BDD_ID b)  { return neg(ite(neg(a), neg(b), FALSE_ID)); }
+    BDD_ID Manager::or2(BDD_ID a, BDD_ID b)  { return ite(a, TRUE_ID, b); }
     BDD_ID Manager::xor2(BDD_ID a, BDD_ID b) { return or2(and2(neg(a),b),and2(a,neg(b))); }
     BDD_ID Manager::neg(BDD_ID a)            { return ite(a, FALSE_ID, TRUE_ID); }
     BDD_ID Manager::nand2(BDD_ID a, BDD_ID b){ return neg(and2(a, b)); }
